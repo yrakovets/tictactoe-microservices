84c54f57e8f5448ffd7b412dc4b30880b7fff8af
Yurii <yrakovets@gmail.com>
2023-08-07 22:00:23 +0300

    added ui microcontroller

---
+++ tic-tac-toe-gateway/src/main/java/com/yrakovets/tictactoe/tictactoegateway/TicTacToeGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.yrakovets.tictactoe.tictactoegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTacToeGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicTacToeGatewayApplication.class, args);
+	}
+
+}
---
+++ tic-tac-toe-gateway/src/test/java/com/yrakovets/tictactoe/tictactoegateway/TicTacToeGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yrakovets.tictactoe.tictactoegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
